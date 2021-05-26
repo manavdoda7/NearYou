be02adc90fa15de6016c7dc998908cbf6d41105b
Manav Doda <manavdoda7@gmail.com>
2021-05-26 20:11:33 +0530

    Homepage elements added

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import './button.css'
 
 const Button = ({value,type})=>{
-    return <>
-        <button className="main-btn" type={type}>{value}</button>
-    </>;
+    return <button className="main-btn" type={type}>{value}</button>;
 }
 
 export default Button;
