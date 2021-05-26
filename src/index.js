479dc1701d202d750813e5d97de339a90846e2aa
Manav Doda <manavdoda7@gmail.com>
2021-05-26 16:01:21 +0530

    Initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Bootstrap Bundles
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import App from './App'
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Carousel = () => {
+    return <h1 className = "h5 mx-3 my-3">Hello World</h1>
+}
+
+ReactDOM.render(<Carousel/>, document.getElementById('root'));
