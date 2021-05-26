70d03324a70997ebc86e63ca24878dc947996385
Manav Doda <manavdoda7@gmail.com>
2021-05-27 02:32:18 +0530

    Homepage Complete

---
+++ src/pageRenders/auth.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../CSS/auth.css'
+
+const Auth = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Auth
