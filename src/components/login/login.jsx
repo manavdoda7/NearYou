d06e420e001fc2af9b4583f7b7cd8fb2ae225386
Manav Doda <manavdoda7@gmail.com>
2021-05-28 11:11:50 +0530

    Login page added

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React from "react";
+import Input from "../input/input";
 
 const Login = () => {
-    return (
-        <React.Fragment>
-            <h1>
-                HI, this is the login page
-            </h1>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div className="authBody">
+        <article>
+          <h1 className="shopregh">Shop Login</h1>
+          <form className="authform">
+            <div>
+              <div className="authflex">
+              <Input
+                hfor="shop_phone_number"
+                type="tel"
+                label="Phone Number"
+              />
+              </div>
+              <div className="authflex">
+              <Input
+                hfor="shop_password"
+                type="password"
+                label="Password"
+              />
+              </div>
+
+              <div className="authflex">
+                <button className="yelbtn" type="submit">
+                  Login
+                </button>
+                <button type="button" className="redbtn">
+                  Registration
+                </button>
+              </div>
+            </div>
+          </form>
+        </article>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Login;
