5bd1250b33cfd9043539c65e47543086b6962b8e
dhruvinfo28 <dhruv.info28@gmail.com>
2021-06-03 18:50:22 +0530

    users can now register

--- src/components/userRegister/userRegister.jsx
+++ src/components/userRegister/userRegister.jsx
@@ -1,12 +1,66 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Input from "../input/input";
+import {url} from '../../backend'
 // user_name`,`user_email`,`user_password`,`user_pincode`,`user_address`
 const Auth = () => {
 
-  const registerUser = (e) => {
-    e.preventDefault();
-      
+  const [wait,setWait] = useState(false);
+  const [inputVals, setInputVals] = useState({
+    user_name:"",
+    user_email:"",
+    user_address:"",
+    user_pincode:"",
+    user_password:"",
+  })
+
+  const valueHandler = (name,val)=>{
+    setInputVals({
+      ...inputVals,
+      [name]: val
+    })
+  }
+
+  const registerUser = (e)=>{
+    setWait(true);
+      e.preventDefault();
+      const data = {
+        user_name: inputVals.user_name,
+        user_email: inputVals.user_email,
+        user_password: inputVals.user_password,
+        user_pincode: inputVals.user_pincode,
+        user_address:inputVals.user_address
+      }
+      console.table(data);
+      setInputVals({
+        user_name:"",
+        user_email:"",
+        user_address:"",
+        user_pincode:"",
+        user_password:"",
+      })
+      axios.post((url+'/user/register').toString(),data)
+        .then(response=>{
+            console.log(response);
+            setWait(false);
+            window.location.href = '/user/login'
+          })
+          .catch(err=>{
+            console.log('here1')
+            if(err.response){
+              if(err.response.status === 409){
+                setWait(false);
+                alert('User already registered');
+                window.location.href = '/user/register'
+              }else{
+                alert('Please retry at some other time')
+                window.location.reload();
+              }
+            }else{
+              alert('Please retry at some other time')
+              window.location.reload();
+            }
+        })
   }
   
   return (
@@ -16,11 +70,14 @@ const Auth = () => {
         <form className="authform" onSubmit={registerUser}>
           <div>
             <div className="authflex">
-                <Input hfor="user_name" type="text" label="Name" />
+                <Input hfor="user_name" type="text" label="Name" value={inputVals.user_name}
+                setValue={valueHandler} />
               <Input
                 hfor="user_email"
                 type="email"
                 label="Email Address"
+                value={inputVals.user_email}
+                setValue={valueHandler}
               />
             </div>
             <div className="authflex">
@@ -28,11 +85,15 @@ const Auth = () => {
                 hfor="user_password"
                 type="password"
                 label="Password"
+                value={inputVals.user_password}
+                setValue={valueHandler}
               />
               <Input
                 hfor="user_pincode"
                 type="number"
                 label="Pincode"
+                value={inputVals.user_pincode}
+                setValue={valueHandler}
               />
             </div>
             <div className="authflex">
@@ -40,6 +101,8 @@ const Auth = () => {
                 hfor="user_address"
                 type="text"
                 label="Address"
+                value={inputVals.user_address}
+                setValue={valueHandler}
               />
             </div>
             <div className="authflex">
