cbaf04a25bb493358e48285edd4b38969f61dfe5
dhruvinfo28 <dhruv.info28@gmail.com>
2021-06-05 15:26:35 +0530

    fetch shops by category, customer home

--- src/backend.js
+++ src/backend.js
@@ -1,1 +1,2 @@
-export const url = 'https://dry-stream-30251.herokuapp.com/api';
+// export const url = 'https://dry-stream-30251.herokuapp.com/api';
+export const url = 'http://localhost:5000/api';
